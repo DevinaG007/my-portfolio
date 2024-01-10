de237267dfac00ac0ea7db89825d623836a1a1d8
Devina Gillis <devinagillis0@gmail.com>
2024-01-09 22:25:15 -0800

    made styling adjustments to header

--- src/home/Header.js
+++ src/home/Header.js
@@ -13,14 +13,14 @@ export default function Header() {
     <>
       <div className="header">
         <h1 className="headings" onClick={handleClick}>Devina Gillis</h1>
-        <div className="nav">
-          <h2 className="headings">Software Engineer</h2>
+        {/* <div className="nav"> */}
+          {/* <h2 className="headings">Software Engineer</h2> */}
           <div className="navigation">
             <NavLink to ="/about" className="navLinks">About Me</NavLink>
             <NavLink to="/projects" className="navLinks">Projects</NavLink>
             <NavLink to="/contact" className="navLinks">Contact</NavLink>
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </>
   );
--- src/home/Introduction.js
+++ src/home/Introduction.js
@@ -10,9 +10,9 @@ export default function Introduction() {
       <h2>I'm a Full-Stack Software Engineer</h2>
       <p>I use JavaScript, Node, React, Express and SQL to develop scalable web applications.</p>
     </div>
-    <div className="column">
+    {/* <div className="column">
       <img src={profile} alt="Devina posing at a lake in Norway"/>
-    </div>
+    </div> */}
     </main>
   );
 }
