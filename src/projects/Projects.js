b21d7f4f8b5ce57766d96065dc4d730f58c3f626
Devina Gillis <devinagillis0@gmail.com>
2024-01-20 19:22:58 -0800

    added styling to projects page

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,26 +1,26 @@
 import React from "react";
 import projects from "../data/projects";
 import "./Projects.css";
-import deck from "../images/flash-card-deck.png"
 
 
 export default function Projects(){
 
    const projectList = projects.map((project) => (
-        <li>
-            <h4>{project.name}</h4>
-            <img src={require("../images/" + project.image_url + ".png")}/>
-            <p>Description: {project.description}</p>
+        <li className="project-list">
+            <a href={project.link}><h4>{project.name}</h4> </a>
+                <img className="project-images" src={require("../images/" + project.image_url + ".png")} alt={project.name}/>
+            <p className="description">Description: {project.description}</p>
             <p>Technologies: {project.technologies}</p>
-            <a src={project.link}>Live Site</a>
         </li>
    ))
     return (
         <main>
+            <section className="projects">
         <h2>Projects</h2>
         <ul>
         {projectList}
         </ul>
+        </section>
         </main>
     )
 };
