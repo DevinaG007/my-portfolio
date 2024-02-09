d136f36e7b6c3c13285000fb9ec5e2ef1d5c9d2e
Devina Gillis <devinagillis0@gmail.com>
2024-02-09 10:13:15 -0800

    added new project and updated broken links

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,20 +2,28 @@
 
 const projects = [
     {
-        "name": "Flashcard App",
-        "link": "www.onetwothree.com",
-        "technologies":"React, JavaScript, Bootstrap, CSS5",
-        "completion_date": "09/2023",
-        "description":"A flashcard application for students to use to study. Built using React, JavaScript, CSS and Bootstrap. Used React Hooks and React Router to build a dimensional website.",
-        "image_url":"flash-card-deck"
-    },
-    {
         "name": "Restaurant Reservation System",
         "link": "https://restaurant-reservations-xxdy.onrender.com/",
         "technologies":"React, JavaScript, CSS, Node.js, Express, PostgreSQL",
         "completion_date": "09/2023",
         "description":"A restaurant reservation application designed to allow restaurant employees an easy method for managing reservations and tables in a restaurant. Full-Stack application built with PostgreSQL, Express, Node, React, JavaScript and CSS.",
         "image_url":"reservation-dashboard"
+    },
+    {
+        "name": "We Love Movies App",
+        "link": "https://we-love-movies-61rs.onrender.com/",
+        "technologies":"JavaScript, Node.js, Express, PostgreSQL",
+        "completion_date": "09/2023",
+        "description":"An application designed for movie lovers to find which movies are currently playing in theaters, and read reviews of those movies. Designed a REST API structure for the backend to ensure clean and quality code.",
+        "image_url":"we-love-movies-app"
+        },
+    {
+        "name": "Flashcard App",
+        "link": "https://flash-card-app-weld.vercel.app/",
+        "technologies":"React, JavaScript, Bootstrap, CSS5",
+        "completion_date": "09/2023",
+        "description":"A flashcard application for students to use to study. Built using React, JavaScript, CSS and Bootstrap. Used React Hooks and React Router to build a dimensional website.",
+        "image_url":"flash-card-deck"
     },
 ]
 
