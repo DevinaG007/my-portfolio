b4bf8e6835e26baf114ffaa0bc731def2e979ffc
Devina Gillis <devinagillis0@gmail.com>
2024-02-10 22:39:47 -0800

    added technology icons to introduction page

--- src/home/Introduction.js
+++ src/home/Introduction.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Introduction.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare, faJs, faNodeJs, faHtml5, faCss3, faReact } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Introduction() {
@@ -11,6 +11,13 @@ export default function Introduction() {
     <div className="desc">
       <h1>Devina Gillis</h1>
       <h2>Full-Stack Software Engineer</h2>
+      <div className="technologies">
+      <FontAwesomeIcon className="icon" icon={faJs} size="1x"/>
+      <FontAwesomeIcon className="icon" icon={faNodeJs} size="1x"/>
+      <FontAwesomeIcon className="icon" icon={faHtml5} size="1x"/>
+      <FontAwesomeIcon className="icon" icon={faCss3} size="1x"/>
+      <FontAwesomeIcon className="icon" icon={faReact} size="1x"/>
+      </div>
       <div>
         <a href="https://www.linkedin.com/in/devina-gillis/">
       <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x"/>
