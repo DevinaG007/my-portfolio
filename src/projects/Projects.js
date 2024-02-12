8ef4b3165a74c809b5dbd5f747e33a3a7696d09f
Devina Gillis <devinagillis0@gmail.com>
2024-02-11 21:00:55 -0800

    added media queries to projects page for responsiveness

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -7,10 +7,10 @@ export default function Projects(){
 
    const projectList = projects.map((project) => (
         <li className="project-list">
-            <a href={project.link}><h4>{project.name}</h4> </a>
+            <a className="description" href={project.link}><h4>{project.name}</h4> </a>
                 <img className="project-images" src={require("../images/" + project.image_url + ".png")} alt={project.name}/>
             <p className="description">Description: {project.description}</p>
-            <p>Technologies: {project.technologies}</p>
+            <p className="description">Technologies: {project.technologies}</p>
         </li>
    ))
     return (
